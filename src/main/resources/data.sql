4330a1c4b2f415217374c79849fa833fd0338f28
MarcinPekala <marcin_pekala_97@icloud.com>
2022-06-06 16:15:22 +0200

    Update

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,9 @@
 drop table BOOK;
 create table book(id bigint auto_increment, AUTHOR varchar(255), TITLE varchar(255), YEAR_OF_PUBLICATION integer);
 INSERT INTO BOOK (id, author, title, year_of_publication) VALUES
-(101L, 'Barack Obama', 'A Promised Land', 2020),
-(102L , 'Bill O''Neill', 'Interesting Stories For Curious People: A Collection of Fascinating Stories About History, Science, Pop Culture and Just About Anything Else You Can Think of', 2020),
-(103L, 'Matthew McConaughey', 'Greenlights', 2020),
-(104L, 'M Prefontaine', 'Difficult Riddles For Smart Kids: 300 Difficult Riddles And Brain Teasers Families Will Love (Books for Smart Kids)', 2017),
-(105L, 'Glennon Doyle', 'Untamed', 2020);
+(101L, 'Colleen Hoover', 'It Ends with Us', 2016),
+(102L , 'Delia Owens', Where the Crawdads Sing, 2021),
+(103L, 'Colleen Hoover', 'Verity', 2022),
+(104L, 'Taylor Jenkins Reid', 'The Seven Husbands of Evelyn Hugo', 2018),
+(105L, 'James Clear', 'Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones', 2018);
 
